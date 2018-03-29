9c88de1989a32b6e435e78ace8f7fd4a30372df8
dmitry <dmitry.mx.gerenrot@intel.com>
2018-03-28 22:26:36 -0700

    Removing unused code

--- nsfc-plugin/src/main/java/org/osc/controller/nsfc/utils/RedirectionApiUtils.java
+++ nsfc-plugin/src/main/java/org/osc/controller/nsfc/utils/RedirectionApiUtils.java
@@ -16,18 +16,10 @@
  *******************************************************************************/
 package org.osc.controller.nsfc.utils;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static java.util.Collections.singletonList;
-import static java.util.stream.Collectors.toSet;
-
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import org.openstack4j.api.Builders;
 import org.openstack4j.model.network.Port;
@@ -35,10 +27,6 @@ import org.openstack4j.model.network.ext.FlowClassifier;
 import org.openstack4j.model.network.ext.PortChain;
 import org.openstack4j.model.network.ext.PortPair;
 import org.openstack4j.model.network.ext.PortPairGroup;
-import org.osc.controller.nsfc.entities.NetworkElementImpl;
-import org.osc.controller.nsfc.entities.PortPairElement;
-import org.osc.controller.nsfc.entities.PortPairGroupElement;
-import org.osc.controller.nsfc.entities.ServiceFunctionChainElement;
 import org.osc.sdk.controller.element.InspectionPortElement;
 import org.osc.sdk.controller.element.NetworkElement;
 import org.slf4j.Logger;
@@ -52,90 +40,6 @@ public class RedirectionApiUtils {
 
     public RedirectionApiUtils(OsCalls osCalls) {
         this.osCalls = osCalls;
-    }
-
-    private NetworkElementImpl fetchNetworkElementWithChildDepends(Port port, String parentId) {
-        checkArgument(port != null, "null passed for %s !", "OS Port");
-
-        List<String> ips = new ArrayList<>();
-        if (port.getFixedIps() != null) {
-            ips = port.getFixedIps().stream().map(ip -> ip.getIpAddress()).collect(Collectors.toList());
-        }
-        return new NetworkElementImpl(port.getId(), singletonList(port.getMacAddress()), ips, parentId);
-    }
-
-    private PortPairElement fetchPortPairElementWithChildDepends(PortPair portPair) {
-        checkArgument(portPair != null, "null passed for %s !", "Port Pair");
-
-        Port ingressPort = portPair.getIngressId() != null ? this.osCalls.getPort(portPair.getIngressId())
-                            : null;
-        Port egressPort = portPair.getEgressId() != null ? this.osCalls.getPort(portPair.getEgressId())
-                            : null;
-
-        NetworkElementImpl ingressElement = null;
-        if (ingressPort != null) {
-            ingressElement = fetchNetworkElementWithChildDepends(ingressPort, portPair.getId());
-        }
-
-        NetworkElementImpl egressElement = null;
-        if (egressPort != null) {
-            egressElement = fetchNetworkElementWithChildDepends(egressPort, portPair.getId());
-        }
-
-        return new PortPairElement(portPair.getId(), null,
-                                        ingressElement, egressElement);
-    }
-
-    private PortPairGroupElement fetchPortPairGroupWithChildDepends(PortPairGroup portPairGroup) {
-        checkArgument(portPairGroup != null, "null passed for %s !", "Port Pair Group");
-        PortPairGroupElement retVal = new PortPairGroupElement(portPairGroup.getId());
-
-        if (portPairGroup.getPortPairs() != null) {
-            List<? extends PortPair> portPairs = this.osCalls.listPortPairs();
-            portPairs = portPairs
-                         .stream()
-                         .filter(pp -> portPairGroup.getPortPairs().contains(pp.getId()))
-                         .collect(Collectors.toList());
-
-            for (PortPair portPair : portPairs) {
-                try {
-                    PortPairElement portPairElement = fetchPortPairElementWithChildDepends(portPair);
-                    retVal.getPortPairs().add(portPairElement);
-                    portPairElement.setPortPairGroup(retVal);
-                } catch (IllegalArgumentException e) {
-                    LOG.error("Port Pair {} listed for port pair group {} does not exist!", portPair.getId(),
-                              portPairGroup.getId());
-                }
-            }
-        }
-
-        return retVal;
-    }
-
-    public ServiceFunctionChainElement fetchSFCWithChildDepends(PortChain portChain) {
-
-        ServiceFunctionChainElement retVal =  new ServiceFunctionChainElement(portChain.getId());
-
-        if (portChain.getPortPairGroups() != null) {
-            Set<? extends PortPairGroup> portPairGroups = new HashSet<>(this.osCalls.listPortPairGroups());
-            portPairGroups = portPairGroups
-                                 .stream()
-                                 .filter(pp -> portChain.getPortPairGroups().contains(pp.getId()))
-                                 .collect(toSet());
-
-            for (PortPairGroup portPairGroup : portPairGroups) {
-                try {
-                    PortPairGroupElement portPairGroupElement = fetchPortPairGroupWithChildDepends(portPairGroup);
-                    retVal.getPortPairGroups().add(portPairGroupElement);
-                    portPairGroupElement.setServiceFunctionChain(retVal);
-                } catch (IllegalArgumentException e) {
-                    LOG.error("Port Pair Group {} listed for port chain group {} does not exist!", portPairGroup.getId(),
-                              portChain.getId());
-                }
-            }
-        }
-
-        return retVal;
     }
 
     public Port fetchProtectedPort(FlowClassifier flowClassifier) {
